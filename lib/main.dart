f249504ef0b15b8bb8b9cfbf13d81f0b327b79a3
artha <143577947+arthaa6days@users.noreply.github.com>
2026-03-01 23:04:52 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:aplikasi_manajemen_tiket_konser/page/home_page.dart';
+
+void main() {
+  runApp(const KonserApp());
+}
+
+class KonserApp extends StatelessWidget {
+  const KonserApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Konser Island',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+
+
